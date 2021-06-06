5e34a4ed13752634180eaf24633d4e8bd9179c8b
Fabién Kouwenberg <fabienkouw@gmail.com>
2021-06-06 14:14:21 +0200

    Setup

---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recipe", menuName = "New Recipe", order = 0)]
+public class Recipe : ScriptableObject
+{
+    public enum Kitchen
+    {
+        Italian,
+        Eastern,
+        Mexican,
+        Dutch,
+        JamieOliver,
+        AllerHande,
+        Easy
+    }
+
+    [SerializeField] private string name;
+    [SerializeField] private Kitchen kitchen;
+    [SerializeField] private string[] ingredients;
+}
